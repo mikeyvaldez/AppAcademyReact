a6ac43e92ba16bede9d9aa73f888ade36f6df7bf
Michael Valdez <mikeyvaldez92@yahoo.com>
2023-02-25 20:17:46 -0800

    functional components

---
+++ ReactFunctionalComponents/functional-components/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css"
+import Showcase from "./Showcase";
+
+function App() {
+  return (
+    <div className="background">
+      <Showcase />
+    </div>
+  );
+}
+
+export default App;
