d662d481161aeb3c854377140792f7b4e97d895b
Michael Valdez <mikeyvaldez92@yahoo.com>
2023-03-04 10:04:26 -0800

    custom hooks long practice

---
+++ HooksCustom-LongPractice/hooks-custom-longpractice/src/components/ProductView/ProductView.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import ProductListItem from "../ProductListItem";
+import ProductDetails from "../ProductDetails";
+import './ProductView.css'
+
+function ProductView({ products }) {
+
+  // TODO: Replace with state variable
+  const [sideOpen, setSideOpen] = useState(true);
+  const [selectedProduct, setSelectedProduct] = useState('')
+
+  return (
+    <div className="product-view">
+      <div className="product-main-area">
+        <h1>Products</h1>
+        <div className="product-list">
+          {products.map(item =>
+            <ProductListItem
+              key={item.id}
+              product={item}
+              isSelected={selectedProduct.id && item.id ? true : false}
+              onClick={() => setSelectedProduct(item)}
+            />
+          )}
+        </div>
+      </div>
+      <div className="product-side-panel">
+        <div className="product-side-panel-toggle-wrapper">
+          <div className="product-side-panel-toggle"
+               onClick={() => setSideOpen(!sideOpen)}>
+            {sideOpen ? '>' : '<'}
+          </div>
+        </div>
+        <ProductDetails visible={sideOpen} product={selectedProduct} />
+      </div>
+    </div>
+  );
+}
+
+export default ProductView;
